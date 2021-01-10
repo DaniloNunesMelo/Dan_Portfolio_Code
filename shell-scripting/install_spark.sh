ed094d34acc985c56235bb497e05f1b23261782f
DaniloNunesMelo <nmelodan@gmail.com>
2021-01-10 15:31:34 +0100

    Unit Test TODO Env Var configuration

--- shell-scripting/install_spark.sh
+++ shell-scripting/install_spark.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Danilo Nunes Melo
 # Creation date 01/08/2021
-# Update date 01/09/2021
+# Update date 01/10/2021
 # Spark Installation Automated
 # Version 1.0
 
 if [[ ${EUID} -eq 0 ]]
      then
-       echo "You have root access"
+       echo "===You have root access==="
      else
-       echo "Root permission required to install"
+       echo "===Root permission required to install==="
        exit
 fi
 
@@ -35,9 +35,9 @@ wget ${url_spark}${spk_vrn} -P /tmp
 
 if [[ $? -eq 0 ]]
      then
-       echo "Spark Downloaded successfully"
+       echo "===Spark Downloaded successfully==="
      else
-       echo "Download not completed"
+       echo "===Download not completed==="
        exit
 fi
 
@@ -47,16 +47,16 @@ tar -xvf /tmp/${spk_vrn} -C /usr/local/
 
 if [[ $? -eq 0 ]]
      then
-       echo "Files unzipped successfully"
+       echo "===Files unzipped successfully==="
      else
-       echo "Error while unzipping"
+       echo "===Error while unzipping==="
        exit
 fi
 
 
 if [[ ${spk_vrn} -eq "^pyspark" ]]
      then
-       echo "Starting configuration Pyspark"
+       echo "===Starting configuration Pyspark==="
        ln -s /usr/local/spark3.0/ /usr/local/spark
        chown -RH spark: /usr/local/spark
        sh -c 'chmod +x /usr/local/spark/bin/*.sh'
@@ -64,20 +64,24 @@ if [[ ${spk_vrn} -eq "^pyspark" ]]
 #       export PYSPARK_DRIVER_PYTHON=jupyter
 #       export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
        export PYSPARK_PYTHON=python3
+       export PYTHONPATH=$(ZIPS=("$SPARK_HOME"/python/lib/*.zip); IFS=:; echo "${ZIPS[*]}"):$PYTHONPATH
      else
-       echo "Error while unzipping"
+       echo "Error while configuring"
        exit
 fi
 ## TODO *Verify env varialbles for R Spark
 if [[ ${spk_vrn} -eq ^SparkR ]]
      then
-       echo "Starting configuration SparkR"
+       echo "===Starting configuration SparkR==="
        ln -s /usr/local/SparkR/ /usr/local/spark
        chown -RH spark: /usr/local/SparkR
        sh -c 'chmod +x /usr/local/SparkR/bin/*.sh'
        export SPARK_HOME=/usr/local/SparkR
+#       export SPARKR_DRIVER_R 'R binary executable to use for SparkR shell (default is R).'
+#       export PYSPARK_DRIVER_PYTHON=jupyter
+#       export PYSPARK_DRIVER_PYTHON=jupyter
 
      else
-       echo "Error while unzipping"
+       echo "===Error while configuring==="
        exit
 fi
